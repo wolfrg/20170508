47d82644d7dff91d1612155492008012d5f097fc
wolfrg <fengruigang@163.com>
2018-01-03 22:23:39 +0800

    binary_search

--- uplooking_Python/binary_search.py
+++ uplooking_Python/binary_search.py
@@ -9,7 +9,15 @@
 '''
 
 #定义一个列表
-L = [1,2,3,4,5]
-
+L = [1,2,5,4,3]
+print L
 print "序列的长度为 %d " % len(L)
-print "序列的第一个元素为 %d " % L[0]
+print "序列的第一个元素为 %d ,索引为0" % L[0]
+print "序列的最后一个元素为 %d ，索引为 %d" %(L[len(L) -1],len(L) -1)
+print "序列中间的元素为 %d " % L[0 + (len(L) - 1) - 0]
+#start = 0
+#end = L[len(L) -1]
+#mid = start + (end - start)/2
+#print L[mid]
+#while True:
+
