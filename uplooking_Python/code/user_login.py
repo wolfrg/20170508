f67a7a7237cf5716edf3dbe828f2bd6369d886fc
wolfrg <fengruigang2009@163.com>
2018-01-09 14:48:32 +0800

    mv test4_v1.py to user_login.py

--- uplooking_Python/code/user_login.py
+++ uplooking_Python/code/user_login.py
@@ -24,7 +24,7 @@ while True:
             else:
                 count = count + 1
                 print "密码错误 %s 次" %  count
-                if count > 3:
+                if count == 3:
                     print "错误超过3次，帐号被锁定"
         else:
             print "用户名不存在，请注册"
