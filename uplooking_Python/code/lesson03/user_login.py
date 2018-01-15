2d781f63e9197f750169defe6b5e0a6c68f138ca
wolfrg <fengruigang2009@163.com>
2018-01-15 17:43:58 +0800

    user_login.py

--- uplooking_Python/code/lesson03/user_login.py
+++ uplooking_Python/code/lesson03/user_login.py
@@ -1,60 +1,51 @@
-#encoding:utf8
-#把之前的代码改为函数形式
-users = {}  #用户字典
+#coding:utf8
+
+users = {} #存储用户信息的字典
+status = {} #{username:count} #记录用户的登录状态
 count = 0
-status = {} #{username,count}
 
-#功能写在函数里面
-#用户登陆的功能
-def user_login(username,password):
-    if users.keys
-
-
-
-
-
+def user_login():
+    print "########请登录###########"
+    username = raw_input("请输入用户名：")
+    if users.has_key(username):
+        password = raw_input("请输入密码：")
+    else:
+        print "用户不存在，请注册"
+        user_register()   #调用注册的函数
 
 
-
-
-
-
-
-
-
-
-#用户注册
 def user_register():
-    username = raw_input("请输入你要注册的用户名:")
-    if username in my_dict:
-        print "用户已经注册"
-    if username not in my_dict:
-        password = raw_input("请输入密码")
-        my_dict[username] = password
-        print "注册成功"
-        print my_dict
-
-def reset_password():
-    pass
+    username = raw_input("请输入用户名：")
+    password = raw_input("请输入密码：")
+    users[username] = password
+    print users
+    print "恭喜您，注册成功！"
 
 
-#逻辑写到这里
+
+def change_password(username):
+    choice = raw_input("是否修改密码(y/N)：")
+    if choice.lower() == 'y':
+        while True:
+            password = raw_input("请输入密码：")
+            password_again = raw_input("请再输入一次密码：")
+            if password == password_again:
+                users[username] = password
+                print "Hello %s 您的密码修已改成功" % username
+                break
+        return True
+
+    else:
+        print "您没有选择修改密码，请继续浏览"
+        exit(-1)
+
+
 def main():
-    while True:
-        user_input = raw_input("L登陆 R注册 Q退出：")
-        if user_input.lower() == "l":
-            user_login()
-
-        if user_input.lower() == 'r':
-            user_register()
-
-        if user_input.lower() == 'q':
-            user_quit()
+    user_login()
+    user_register()
+    user_login()
 
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
-
-
-
